bd748b34b5d20d8d0e4f407749d89c857ba208ef
1N5P3CT0R <akrivosheya@nasty-cat.com>
2023-06-26 16:50:18 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'He11B1azzzer.github.io';
+}
