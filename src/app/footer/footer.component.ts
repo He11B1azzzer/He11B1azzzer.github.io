3187b55d4535f08d3ce2f34eef85ad066474c8e6
1N5P3CT0R <akrivosheya@nasty-cat.com>
2023-06-28 01:46:28 +0300

    test implementation

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {AppBase} from "../app-base";
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent extends AppBase {}
