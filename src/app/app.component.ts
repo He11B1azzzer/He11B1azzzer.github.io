ec83f567a1f130f06c9cc8fb4eb41ac0896e9d91
1N5P3CT0R <akrivosheya@nasty-cat.com>
2023-06-28 16:11:06 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {DataExportService} from "../data";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public data$ = this.dataExportService.getData();
